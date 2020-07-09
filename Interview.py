bc114dc2eb3c8e8b26cdaa7acc8bfd6b149079e7
Shreya <58472791+Shreya010@users.noreply.github.com>
2020-07-09 14:24:08 +0530

    Add files via upload
    Coding Challenge Set - B Solution

---
+++ Interview.py
@@ -0,0 +1,21 @@
+def minimumCost(CostList, People):
+    CostList = sorted(CostList)
+    totalCost = 0
+    for person in range(People - 1, 1, -2):
+        if (person == 2):
+            totalCost += CostList[2] + CostList[0]
+        else:
+            price_first = CostList[person] + CostList[0] + 2 * CostList[1]
+            price_second = CostList[person] + CostList[person - 1] + 2 * CostList[0]
+            totalCost += min(price_first, price_second)
+    if (People == 1):
+        totalCost += CostList[0]
+    else:
+        totalCost += CostList[1]
+    return totalCost
+TestCases = int(input("Enter the number of test cases"))
+for testCases in range(TestCases):
+    CostList = [int(cost) for cost in input("Enter the prices").split(",")]  # [30, 40, 60, 70]
+    People = len(CostList)
+    print(minimumCost(CostList, People))
+
